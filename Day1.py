a2842a4997e6462448f22d6ee526d92f8712fad0
Da'Von Manuel <160780050+Davon112@users.noreply.github.com>
2024-04-29 16:51:27 -0500

    Add files via upload

---
+++ Day1.py
@@ -0,0 +1,44 @@
+# Task 1 Code Correction
+
+weather = "sunny"
+
+if weather == "sunny":
+    print("Wear sunglasses!")
+else:
+    print("Take an umbrella!")
+
+
+# Task 2 Your Mood Today
+
+feeling = input("How are you feeling today: ")
+
+if feeling == "happy":
+    print("That's great to hear!")
+if feeling == "sad":
+    print("I hope your day gets better!")
+
+
+
+
+
+# Question 2
+
+pi_value = 3.14
+user_age = 25
+USER_LOCATION = "New York"
+MAX_LIMIT = 1000
+
+# Question 3 Task 1
+
+peanut_butter = 8.32
+eggs = 9.33
+steak = 15.11
+Total_Cost = print(peanut_butter+eggs+steak)
+
+# Question 3 Task 2
+
+savings = 10000
+interest = .07
+eoy_interest = savings*interest
+eoy_total = print(savings+eoy_interest)
+
